240993c0314521d46608bdb5b049fd915855988c
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-09-03 08:12:38 +0200

    feat: added pictures

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Text from './componants/Text/text'
+import {Navbar} from './componants/Navbar/navbar'
 
 function App() {
 
   return (
     <>
+    <Navbar />
     <Text variant='h1' style={{color: 'blue', textAlign: 'center'}}>Hello World</Text>
     <Text variant='h2' style={{color: 'red', textAlign: 'center'}}>Hello World</Text>
     <Text variant='p' style={{color: 'green', textAlign: 'center'}}>Hello World</Text>
--- src/componants/Navbar/navbar.tsx
+++ src/componants/Navbar/navbar.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
-import {Text} from './Text/Text'
-import './navbar.css'
+import { Text } from '../Text/Text'
+import styles './navbar.css'
 
-export default function navbar(props) {
+export const Navbar: React.FC<any> = (props) => {
+    console.log({props});
   return (
-    <div className={StyleSheet.content}>
-      <Text variant="h1" style={{ color: "blue", textAlign: "center" }}>
-        Hello World
-      </Text>
+    <nav>
+    <div className={styles.content}>
+      <Text variant="h1" style={{ color: "blue", textAlign: "center" }}>Hello World</Text>
       <div>
-        <a href="/#" className={StyleSheet.link}>Home</a>
-        <a href="/#" className={StyleSheet.link}>Home</a>
-        <a href="/#" className={StyleSheet.link}>Home</a>
+        <a href="/#" className={styles.link}>Home</a>
+        <a href="/#" className={styles.link}>Home</a>
+        <a href="/#" className={styles.link}>Home</a>
+        <div className={styles['profile-icon']}>
+        <Text variant={'span'}>U</Text>
+        </div>
       </div>
     </div>
+    </nav>
+    
   );
 }
